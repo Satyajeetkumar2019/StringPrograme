6b076c15f8d982d21470e6ac1d121047bb2981c4
satyajeet kumar <55208293+Satyajeetkumar2019@users.noreply.github.com>
2021-02-08 01:32:18 +0530

    Add files via upload

---
+++ String01.java
@@ -0,0 +1,36 @@
+//to string manipulaction 
+import java.util.Scanner;
+class String01 {
+	public static void main(String[] args) {
+Scanner scn= new Scanner(System.in);
+System.out.println("Enter the String :");
+String sn=scn.nextLine();
+//TO String compare 
+if(sn.equals("HK")){
+
+System.out.println("Well come to naresh it ");
+}
+else{
+System.out.println("HOW are you :");
+}
+//if we want to insert more string data then we must be create striung buffer class
+StringBuffer bs=new StringBuffer("satyajeet kumar ");
+System.out.println(bs);
+
+
+
+	String name="Satyejeet kumar";
+//to create string object 
+	System.out.println(name);
+//String na=akjdn;
+//System.out.println(satyajeet);//complile time error
+java.lang. String names="kundan";
+System.out.println(names);
+
+
+
+
+
+		System.out.println("Hello World!");
+	}//end of the main method 
+}//end of the class
